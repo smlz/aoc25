2f168d3590b90240968dc23eb379f3dc8c22174d
Marco Schmalz <marco@schess.ch>
2025-12-05 15:17:53 +0100

    day 3: remove dead code

--- day3.py
+++ day3.py
@@ -7,8 +7,6 @@ for line in lines:
     first = max(numbers[:-1])
     idx = numbers.index(first)
     second = max(numbers[numbers.index(first) + 1:])
-    #print(second)
-    #print(f"{nbr=}")
     res += 10 * first + second
 
 print("part 1:", res)
@@ -18,8 +16,6 @@ print("part 1:",
             for line in lines if (numbers := [int(x) for x in line])
          )
      )
-
-5322532524412431424224553433255235332456444432543425324265123263732352855273513551554412243351423435
 
 res = 0
 for line in lines:
@@ -31,8 +27,6 @@ for line in lines:
         res += digit * 10**i
 
 print("part 2:", res)
-#part 2: 175053592950232
-
 
 print("part 2:",
       sum(sum(digit * 10**(11-i) for i in range(12) if (digit := max(numbers[idx:(len(numbers) - 11 + i)])) >= 0 and (idx := idx + numbers[idx:].index(digit) + 1) >= 0)
