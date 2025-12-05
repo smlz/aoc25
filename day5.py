8a7a459555fff157a6b5a186c0f45b23ab7b3dd8
Marco Schmalz <marco@schess.ch>
2025-12-05 15:18:51 +0100

    day 5: nested list comprehensions / generator expressions

--- day5.py
+++ day5.py
@@ -1,7 +1,6 @@
 import pathlib
 ranges, ids = (pathlib.Path(__file__).parent / "day5.txt").read_text().strip().split("\n\n")
-ranges = (line.split("-") for line in ranges.split("\n"))
-ranges = [range(int(i), int(j) + 1) for i, j in ranges]
+ranges = [range(int(i), int(j) + 1) for i, j in (line.split("-") for line in ranges.split("\n"))]
 
 print("part 1:", sum(any(int(i) in r for r in ranges) for i in ids.split("\n") if i.strip()))
 
@@ -14,7 +13,7 @@ for r in ranges:
         old_r = r
     elif old_r.stop < r.stop:                 # overlap, but not contained
         old_r = range(old_r.start, r.stop)    # extend old range
-    # else:                                     contained, thus throw away
+    # else:                                   # contained, thus throw away
     
 # append last range
 new_ranges.append(old_r)
