207af7236403e56065b67eae4f6ed96cf53a13b2
Marco Schmalz <marco@schess.ch>
2025-12-05 15:16:49 +0100

    day 1: clean up

--- day1.py
+++ day1.py
@@ -1,20 +1,6 @@
 import pathlib
 codes = (pathlib.Path(__file__).parent / "day1.txt").read_text().strip().split("\n")
 
-ccodes = """L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82""".split("\n")
-
-p = print
-print = lambda *x: ...
-
 x = 50
 n1 = 0
 n2 = 0
@@ -37,18 +23,19 @@ for l, line in enumerate(codes, 1):
     if x == 0:
         n1 += 1
        
-p("Answer part 1:", n1)
-p("Answer part 2:", n2)
+print("part 1:", n1)
+print("part 2:", n2)
 
 
-print = p
+# Brute-force one-liners using generator expressions and walrus operator
 x = 50
-print(sum((x := (x + (-1 if c[0] == "L" else 1) * int(c[1:])) % 100) == 0 for c in codes))
+print("part 1:", sum((x := (x + (-1 if c[0] == "L" else 1) * int(c[1:])) % 100) == 0 for c in codes))
 
 x = 50
-print(sum((x := (x + (-1 if c[0] == "L" else 1)) % 100) == 0 for c in codes for _ in range(int(c[1:]))))
+print("part 2:", sum((x := (x + (-1 if c[0] == "L" else 1)) % 100) == 0 for c in codes for _ in range(int(c[1:]))))
 
 
+# generator function
 def all_clicks(start, codes):
     x = start
     for code in codes:
@@ -60,13 +47,3 @@ def all_clicks(start, codes):
 
 
 print(sum(n == 0 for n in all_clicks(50, codes)))
-
-p1, p2, x = 0, 0, 50
-for line in codes:
-    for i in range(int(line[1:])):
-        x = (x + [-1, 1][line[0] == 'R']) % 100
-        p2 += x == 0
-    p1 += x == 0
-
-p(p1)
-p(p2)
