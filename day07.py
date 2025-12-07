6da7f0d3a0ebe7bc6bab32d62434ebc9119aed61
Marco Schmalz <marco@schess.ch>
2025-12-07 11:39:33 +0100

    day 7: part 2

--- day07.py
+++ day07.py
@@ -15,3 +15,20 @@ for line in lines[1:]:
             nbr += 1
 
 print("part 1:", nbr)
+
+
+positions = {lines[0].index("S")}
+beam_counts = [1 if c == "S" else 0 for c in lines[0]]
+
+for line in lines[1:]:
+    for pos in list(positions):
+        if line[pos] == "^":
+            positions.remove(pos)
+            positions.add(pos - 1)
+            positions.add(pos + 1)
+
+            beam_counts[pos - 1] += beam_counts[pos]
+            beam_counts[pos + 1] += beam_counts[pos]
+            beam_counts[pos] = 0
+
+print("part 2:", sum(beam_counts))
