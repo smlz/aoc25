64dabc77cd56ab5dfc8b30db6c09c60f382e963a
Marco Schmalz <marco@schess.ch>
2025-12-08 14:50:14 +0100

    day 8: part 1
    This was harder than usual. Took a wrong turn at some point and forgot
    that two existing circuits should be merged when adding two boxes between
    them.
    
    Often there are some good hints in the problem formulation. Reading helps.
    Also peeking at other's solutions :)
    shoutout @nitekat1124:
    https://github.com/nitekat1124/advent-of-code-2025/blob/main/solutions/day08.py

---
+++ day08.py
@@ -0,0 +1,31 @@
+import pathlib
+data_raw = (pathlib.Path(__file__).parent / "day08.txt").read_text().strip().split()
+
+##########
+# PART 1 #
+##########
+from itertools import combinations
+from math import prod
+
+dist = lambda boxes: sum((n1 - n2) * (n1 - n2) for n1, n2 in zip(*boxes))
+boxes = [tuple(map(int, line.split(","))) for line in data_raw]
+pairs_by_distance = sorted(combinations(boxes, 2), key=dist)
+
+circuits = [{b} for b in boxes]  # as formulated in problem description
+                                 # (also prevents edge cases later)
+
+n = 1000
+for b1, b2 in pairs_by_distance[:n]:
+    i1 = -1
+    i2 = -1
+    for i, c in enumerate(circuits):  # locate circuits for boxes
+        if b1 in c:
+            i1 = i
+        if b2 in c:
+            i2 = i
+
+    if i1 != i2:    # not yet in the same circuit, thus merge circuits
+        circuits[i1] |= circuits[i2]
+        del circuits[i2]
+
+print("part 1:", prod(sorted(map(len, circuits), reverse=True)[:3]))
